b89d983b4a8726e4f8367ffbe0e175a47dbab6a7
rabbitism <popmessiah@126.com>
2018-03-28 02:33:52 +0800

    Updated to Page 3

---
+++ Chapter1/Basic.py
@@ -0,0 +1,18 @@
+# print a Hello World
+print("Hello, world!")
+
+#Numbers and Expressions
+print("Example 1")
+print("2+2")
+print(2+2)
+print("Example 2")
+print("53672+235253")
+print(53672+235253)
+
+#Multiply and Division
+print("Multiply\nExample 1")
+print("1+2*3")
+print(1+2*3)
+print("Example 2")
+print("(1+2)*3")
+print((1+2)*3)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Beginning-Python-Practice
+
+This is my personal practice repo for book Beginning Python From Novice to Professional
