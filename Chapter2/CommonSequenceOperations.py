2d851b1b12506dae1ae524b219fe8bb6501d00b1
rabbitism <popmessiah@126.com>
2018-04-05 02:43:05 +0800

    Update to Page 31

--- Chapter2/CommonSequenceOperations.py
+++ Chapter2/CommonSequenceOperations.py
@@ -4,8 +4,8 @@ print(greeting[0])
 print(greeting[-1])
 #print(greeting[5]) IndexError: string index out of range
 
-fourth = input('Year: ')[3]
-print(fourth)
+#fourth = input('Year: ')[3]
+#print(fourth)
 
 months = [
     'January',
@@ -36,3 +36,27 @@ month_name = months[month_number-1]
 ordinal = day+endings[day_number-1]
 
 print(month_name+' '+ordinal+', '+year)
+
+tag = '<a href="http://www.python.org">Python web site</a>'
+
+print(tag[9:30])
+print(tag[32:-4])
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers[3:6])
+print(numbers[0:1])
+print(numbers[-3:])
+print(numbers[3:])
+print(numbers[0:3])
+print(numbers[:])
+print(numbers[0:10:1])
+print(numbers[0:10:2])
+print(numbers[3:6:3])
+print(numbers[::4])
+print(numbers[::-1])
+
+#Adding Sequences
+print([1,2,3]+[4,5,6])
+#Multiplication
+print('python'*5)
+
