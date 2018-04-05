1e91db600d19e67cdb84ae8bc71a4e266cd634d2
rabbitism <popmessiah@126.com>
2018-04-06 03:16:51 +0800

    Update to page 62

---
+++ Chapter4/CreatingAndUsingDictionaries.py
@@ -0,0 +1,44 @@
+items = [
+    ('name', 'Gumby'),
+    ('age', 42)
+]
+
+d = dict(items)
+print(d)
+print(d['name'])
+
+x={}
+x[42] = 'Foobar'
+print(x)
+
+#Listing 4-1: Dictionary Example
+# A simple database
+# A dictionary with person names as keys. Each person is represented as another dictionary with the keys 'phone' and 'addr' referring to their phone number and address, respectively
+people = {
+    'Alice':{
+        'phone': '2341',
+        'addr': 'Foo drive 23'
+    },
+    'Beth':{
+        'phone': '9102',
+        'addr': 'Bar street 42'
+    },
+    'Cecil':{
+        'phone': '3158',
+        'addr': 'Baz avenue 90'
+    }
+}
+labels = {
+    'phone': 'phone number',
+    'addr': 'address'
+}
+name = input('Name: ')
+# Are we looking for a phone number or an address?
+request = input('Phone number (p) or address (a)? ')
+
+# Use the correct key:
+if request == 'p': key = 'phone'
+if request == 'a': key = 'addr'
+
+# Only try to print information if the name is a valid key in our dictionary
+if name in people: print("{}'s {} is {}.".format(name, labels[key], people[name][key]))
