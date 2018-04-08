75e0c000d8f8145b323f78372c4382c47ebee02f
rabbitism <popmessiah@126.com>
2018-04-08 08:02:05 +0800

    Update to page 75

---
+++ Chapter5/MoreAboutPrint.py
@@ -0,0 +1,10 @@
+#print multiple argument together
+name = 'Gumby'
+salutation = 'Mr.'
+greeting = 'Hello, '
+print(greeting, salutation, name)
+
+#customize separator
+print('I', 'wish', 'to', 'register', 'a', 'compliant', sep = '_')
+print('Hello, ',end='')
+print('world!')
